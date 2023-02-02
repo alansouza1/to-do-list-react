414f047e8ae347d949dd0d7a018b86e48f8fb11c
alansouza1 <alansouza4001@gmail.com>
2023-02-02 19:02:04 -0300

    feat: Adiciona botão de criar uma conta

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,26 @@
 export default function Login() {
   return (
-    <form method="post">
-      <label htmlFor="email-input">
-        Email
-        <input
-          id="email-input"
-          name="email"
-          type="email"
-        />
-      </label>
-      <label htmlFor="password-input">
-        Senha
-        <input
-          id="password-input"
-          name="password"
-          type="password"
-        />
-      </label>
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <form method="post">
+        <label htmlFor="email-input">
+          Email
+          <input
+            id="email-input"
+            name="email"
+            type="email"
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha
+          <input
+            id="password-input"
+            name="password"
+            type="password"
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <a href="/">Criar uma conta</a>
+    </>
   );
 }
