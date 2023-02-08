d0a083f36f6d7a0a3ef2297ddc166ad75127e42e
alansouza1 <alansouza4001@gmail.com>
2023-02-08 14:31:12 -0300

    feat: Cria componente Header.jsx

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,36 +1,43 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from '../components/Header';
+
+import '../styles/pages/Login.css';
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
     <>
-      <form method="post">
-        <label htmlFor="email-input">
-          Email
-          <input
-            id="email-input"
-            name="email"
-            type="email"
-            value={ email }
-            onChange={ ({ target }) => setEmail(target.value) }
-          />
-        </label>
-        <label htmlFor="password-input">
-          Senha
-          <input
-            id="password-input"
-            name="password"
-            type="password"
-            value={ password }
-            onChange={ ({ target }) => setPassword(target.value) }
-          />
-        </label>
-        <button type="submit">Entrar</button>
-      </form>
-      <Link to="signup">Criar conta</Link>
+      <Header />
+      <div className="login-form">
+        <form method="post">
+          <label htmlFor="email-input">
+            Email
+            <input
+              id="email-input"
+              name="email"
+              type="email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha
+            <input
+              id="password-input"
+              name="password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+        <Link to="signup">Criar conta</Link>
+      </div>
     </>
   );
 }
